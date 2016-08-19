c6d9441220cc754956ae86f4a040af1de4d220c9
andrewrd <andrew@Andrews-MacBook-Air.local>
2016-08-19 13:45:31 +1000

    added basic function definitions

---
+++ scripts.js
@@ -0,0 +1,36 @@
+//returns an array of locations pulls from database or hardcode
+function checkList(){
+	return arrayOfLocation;
+}
+
+//Gets the users location
+function getUserLocation(){
+	return location;
+}
+
+
+//Calculates the location based upon radius of area 
+function compareLocation(gps1, metres){
+	var user = getUserLocation();
+	var locations = checkList();
+	return true;
+}
+
+//Triggers a notification with message if near area
+function notifyUser(message){
+	return "";
+}
+
+
+//Event listener that triggers a conditional if the latitude and longditude is met
+function watchUser(){
+	var getLocations = checkList();
+	//Event listener here that is delayed for phone
+	for(var i=0; i<getLocations.length(); i++){
+		if (compareLocation(getLocations[i], 20){
+			notifyUser();
+		}
+	}
+
+}
+
