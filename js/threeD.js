76a34caa2ffc814fc746662eab92264397940082
Tuckmo <marktuckey.art@gmail.com>
2016-09-08 13:02:01 +1000

    added cleaner rendering code. Added OBJLoader for static files. Added controls to rotate around objects.Added lighting.

--- js/threeD.js
+++ js/threeD.js
@@ -1,33 +1,61 @@
-//Javascript file for the 3d objects.
-//Dependencies: three.js
+if(!Detector.webgl){
+    Detector.addGetWebGlMessage();
+}
 
+var container;
+var camera, controls, scene, renderer;
+var lighting, ambient, keyLight, fillLight, backLight;
+var windowHalfX = window.innerWidth/2;
+var windowHalfY = window.innerHeight/2;
 
-//Setup the basic camera settings, scene and renderer
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+init();
+render();
 
-//select the part of the DOM we want to insert into
-document.getElementById("animatedModal").appendChild(renderer.domElement);
+function init(){
+    camera =  new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.z=3;
+    scene = new THREE.Scene();
+    ambient = new THREE.AmbientLight(0xffffff,1.0);
+    scene.add(ambient);
+    
+    keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30,100%,75%)'),1.0);
+    keyLight.position.set(-100,0,100);
+    
+    fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+    fillLight.position.set(100,0,100);
+    
+    backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+    backLight.position.set(100, 0, -100).normalize();
+    
+    scene.add(keyLight);
+    scene.add(fillLight);
+    scene.add(backLight);
+    
+    var objLoader = new THREE.OBJLoader();
+    objLoader.setPath('obj/');
+    objLoader.load('cube.obj', function(object){
+        scene.add(object);
+    });
+    
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(new THREE.Color("hsl(0, 0%, 10%)"));
+    
+    document.getElementById("animatedModal").appendChild(renderer.domElement);
+    
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.25;
+    controls.enableZoom = false;
+    
 
-//Add geometry
-var geometry = new THREE.BoxGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial( {color: 0x00ff00 } );
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+    
+    
+}
 
-//position camera
-camera.position.z = 5;
-
-//function that acts as a main loop for the animation
-//do logic to models etc here.
 function render(){
     requestAnimationFrame(render);
-    
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
-//call the main loop
-render();
+
