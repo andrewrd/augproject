1bb26a485d5ac6cbfe38c062dae2bfd5b8a9b8a1
JunaidAhmad7 <junaid.ahmad@students.mq.edu.au>
2016-10-17 23:26:25 +1100

    Made dynamic foundLocations page
    still missing link to 3D object

--- js/scripts.js
+++ js/scripts.js
@@ -286,7 +286,8 @@ function watchUserLocation(location) {
                 foundStatues = removeFound(foundLocationNames, statues, foundStatues);
                 //saves cookie each time a location is found
                 saveCookie(foundLocationNames);
-
+                //adds found locations to foundLocations menu
+                //printLocsMenu(foundStatues);
 
                 //creates a google maps LatLng object
 
@@ -335,6 +336,20 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+//adds the found locations onto the FoundLocations menu overlay
+for (var i = foundStatues.length-1; i >= 0; i--) {
+  var foundStatueName = foundStatues[i].name;
+  var foundStatueID = foundStatues[i].id;
+  $( ".overlay-content" ).append("<a href = '#' id = 'nameMenuItem'>" + foundStatueName + "</a>");
+  $( ".overlay-content" ).append("<a href='#' id = 'statueMenuItem'><div class = 'menuItem' id = '" +foundStatueID+ "'>Statue Info</div></a>");
+}
+
+//grabs the id of the FoundLocations menuItem and directs to its information page
+$('.menuItem').click(function() {
+  locationDisplay($(this).attr('id'));
+  closeNav();
+});
 
 //Jquery dependency closes modal
 $("#demo01").animatedModal();
