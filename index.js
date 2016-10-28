151c05bf59ef9aa5656f9b88a530484318e97299
andrewrd <andrewrd@users.noreply.github.com>
2016-10-28 12:02:26 +1100

    added /getmodel req variable

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var request = require('hyperquest');
 
 app.use(express.static(__dirname + '/static'));
 
-app.get('/getmodels', function (req, res) {
-	request('http://3d.ltc.mq.edu.au/3d/model/181/WWII_AK772_TOY_PLANE_10k.obj').pipe(process.stdout);
+app.get('/getmodels/:name', function (req, res) {
+	var name = req.params;
+	var rname = request('http://3d.ltc.mq.edu.au/3d/model/181/WWII_AK772_TOY_PLANE_10k.obj');
+	//Returns json response with name
+	res.send(name);
 });
 
 app.get('/',function(req,res){
